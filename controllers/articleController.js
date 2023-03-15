3d544eb032aed2bf67159d0ab0d970c3a50a4c75
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-03-15 16:01:10 +0530

    article added

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -6,15 +6,34 @@ export const addArticle = async (req, res) => {
 	// console.log(req.files["thumbnail"][0]);
 	// console.log(req.files['featured'][0]);
 	// console.log(req.body,"body");
+	const { heading, readTime, description, categories, status } = req.body;
 	const thumbnail = req.files["thumbnail"][0];
 	const featured = req.files["featured"][0];
+
+	const sql =
+		"INSERT INTO articles(heading,read_time,description,categories,thumbnail,featured,status) VALUES(?,?,?,?,?,?,?)";
 
 	try {
 		const featuredResult = await cloudinary.uploader.upload(featured.path);
 		const thumbnailResult = await cloudinary.uploader.upload(thumbnail.path);
-		res.status(200).json({
+		const addArticle = await db.query(sql, [
+			heading,
+			readTime,
+			description,
+			categories,
+			thumbnailResult.url,
+			featuredResult.url,
+			status
+		]);
+		console.log(addArticle, "addArticle");
+		res.status(201).json({
+			heading,
+			readTime,
+			description,
+			categories,
 			thumbnailURL: thumbnailResult.url,
-			featuredURL: featuredResult.url
+			featuredURL: featuredResult.url,
+			status
 		});
 	} catch (error) {
 		console.log(error);
